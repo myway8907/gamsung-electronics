2a19a7a3b0ef4690996a8485efce0b23bcc8fa81
minuk <alsdnr4320@gmail.com>
2021-12-27 18:22:01 +0900

    Modify: 초기 세팅수정

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>네비</div>;
+};
+
+export default Navbar;
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products = () => {
+  return <div>상품 리스트</div>;
+};
+
+export default Products;
