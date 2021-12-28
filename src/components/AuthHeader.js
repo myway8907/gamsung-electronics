a23fde400e36753b2bac9c349dde3ee59f835995
minuk <alsdnr4320@gmail.com>
2021-12-28 18:47:24 +0900

    Add: Signup 페이지, Login 페이지

---
+++ src/components/AuthHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./auth_header.scss";
+
+const AuthHeader = () => {
+  return (
+    <header className="auth-header">
+      <div>
+        <h2>SAMSUNG</h2>
+        <span>Account</span>
+      </div>
+    </header>
+  );
+};
+
+export default AuthHeader;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AuthHeader from "../../components/AuthHeader";
+import "./login.scss";
 
 const Login = () => {
-  return <div>로그인</div>;
+  return (
+    <>
+      <AuthHeader />
+      <main className="login">
+        <div>
+          <section>
+            <h1>삼성계정으로 로그인</h1>
+            <form>
+              <label htmlFor="email">
+                <input type="email" placeholder="이메일" />
+                <span>이메일</span>
+              </label>
+              <label htmlFor="password">
+                <input
+                  type="password"
+                  placeholder="비밀번호"
+                  autoComplete="off"
+                />
+                <span>비밀번호</span>
+              </label>
+              <button>로그인</button>
+            </form>
+            <Link to="/signup">계정 생성</Link>
+          </section>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Login;
