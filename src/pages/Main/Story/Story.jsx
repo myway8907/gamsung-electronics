e50e253be4efd0f23d638142fa1a9b5e44ea602b
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2021-12-29 13:55:11 +0900

    ADD: Main의 Story 부분 추가

--- src/pages/Main/Story/Story.jsx
+++ src/pages/Main/Story/Story.jsx
@@ -1,7 +1,61 @@
 import "./Story.scss";
 
+const STORY = [
+  {
+    id: 1,
+    url: ".images/Main/Story/Story1.jpg",
+    story: "01 삼성 갤럭시와 메종 키츠네 여우의 만남",
+  },
+  {
+    id: 2,
+    url: ".images/Main/Story/Story2.jpg",
+    story: "02 #함께 만드는 더 나은 미래",
+  },
+  {
+    id: 3,
+    url: ".images/Main/Story/Story3.jpg",
+    story: "03 #우리가 집에서 말하는 새로운 방법",
+  },
+  {
+    id: 4,
+    url: ".images/Main/Story/Story4.jpg",
+    story: "04 #승리를 위한 완벽한 준비",
+  },
+];
+
 const Story = () => {
-  return <section className="main__story" />;
+  return (
+    <section className="main__story">
+      <header className="story__header">
+        <h2>스토리</h2>
+        <p>
+          당신의 삶을 특별하게 해줄 새로운 아이디어
+          <br />
+          스마트하게 사는 새로운 방법을 알아보세요
+        </p>
+      </header>
+      <div className="story__images" style={{ "--current": `${false}` }}>
+        <img src="./images/Main/Story/Story1.jpg" alt="fake story" />
+        <img src="./images/Main/Story/Story2.jpg" alt="fake story" />
+        <img src="./images/Main/Story/Story3.jpg" alt="fake story" />
+        <img src="./images/Main/Story/Story4.jpg" alt="fake story" />
+      </div>
+      <ul className="story__lists">
+        <li className="story" data-idx="1">
+          01 삼성 갤럭시와 메종 키츠네 여우의 만남
+        </li>
+        <li className="story" data-idx="2">
+          02 #함께 만드는 더 나은 미래
+        </li>
+        <li className="story" data-idx="3">
+          03 #우리가 집에서 일하는 새로운 방법
+        </li>
+        <li className="story" data-idx="4">
+          04 #승리를 위한 완벽한 준비
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Story;
