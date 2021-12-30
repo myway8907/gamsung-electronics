abb507492881b5193e3ae443b959fcad03bba4f1
myway8907 <myway8907@gmail.com>
2021-12-30 22:45:04 +0900

    필터 컴포넌트 요소 배치를 기존의 position: absolute를 이용했던 방식에서 flex를 이용한 방식으로 변경

--- src/pages/Products/Filter/ProductsFilter.js
+++ src/pages/Products/Filter/ProductsFilter.js
@@ -5,19 +5,13 @@ const ProductsFilter = () => {
   return (
     <div className="productFilter">
       <span className="product-type-filter">유형</span>
-      {/* <div className="expand-button-container">
-        <div className="plus-button"></div>
-        <div className="plus-button"></div>
-        <div className="plus-button"></div>
-      </div> */}
       <input className="expand-input" id="expand-button" type="checkbox" />
       <label for="expand-button">
-        {/* <div className="expand-button-container"> */}
-        <div className="plus-button 1"></div>
-        <div className="plus-button 2"></div>
-        <div className="plus-button 3"></div>
-        <div className="plus-button 4"></div>
-        {/* </div> */}
+        <div className="expand-button-container">
+          <div className="plus-button 1"></div>
+          <div className="plus-button 2"></div>
+          <div className="plus-button 3"></div>
+        </div>
       </label>
       <div className="filtering-list">
         <li>
