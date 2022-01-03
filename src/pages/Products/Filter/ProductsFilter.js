d8e529eb0d35b5f6ce89099f4a5a69ca6d11bda3
myway8907 <myway8907@gmail.com>
2022-01-03 14:48:57 +0900

    Add: ProductsFilter.js 파일의 plus-button 클래스 명 변경

--- src/pages/Products/Filter/ProductsFilter.js
+++ src/pages/Products/Filter/ProductsFilter.js
@@ -8,9 +8,9 @@ const ProductsFilter = () => {
       <input className="expand-input" id="expand-button" type="checkbox" />
       <label htmlfor="expand-button">
         <div className="expand-button-container">
-          <div className="common-button first-plus-button" />
-          <div className="plus-button 2" />
-          <div className="plus-button 3" />
+          <div className="common-button first-button-element" />
+          <div className="common-button second-button-element" />
+          <div className="common-button third-button-element" />
         </div>
       </label>
       <div className="filtering-list">
