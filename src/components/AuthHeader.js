6c1fad53acd6a25a8c12faf121f6df52e457e2f3
minuk <alsdnr4320@gmail.com>
2022-01-03 11:39:25 +0900

    Add: AuthHeader 컴포넌트 main Link 추가

--- src/components/AuthHeader.js
+++ src/components/AuthHeader.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./auth_header.scss";
 
 const AuthHeader = () => {
   return (
     <header className="auth-header">
       <div>
-        <h2>SAMSUNG</h2>
+        <h2>
+          <Link to="/">SAMSUNG</Link>
+        </h2>
         <span>Account</span>
       </div>
     </header>
