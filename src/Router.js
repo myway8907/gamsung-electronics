729af37c1baa6ef748e277a37ad3b306c20ffa94
minuk <alsdnr4320@gmail.com>
2022-01-03 15:11:19 +0900

    Add: Cart 페이지 라우터에 추가

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import Sign from "./pages/Sign/Sign";
 import Products from "./pages/Products/Products";
+import Cart from "./pages/Cart/Cart";
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/signin" element={<Sign />} />
         <Route path="/signup" element={<Sign />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
