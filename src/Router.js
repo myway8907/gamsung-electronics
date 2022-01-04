a0847deb30343e4001dcff4156efa25ba700e049
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-01-04 15:27:49 +0900

    Fix: Router 주석 해제

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Main from "./pages/Main/Main";
-// import Login from "./pages/Login/Login";
-// import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 import Products from "./pages/Products/Products";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
 
@@ -11,8 +11,8 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/signup" element={<Signup />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetail />} />
       </Routes>
