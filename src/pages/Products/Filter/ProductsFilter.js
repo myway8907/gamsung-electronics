6161e6965da6e004efe77a4b86b06e5a459a582a
myway8907 <myway8907@gmail.com>
2022-01-05 17:23:11 +0900

    중간저장용: 실제 내용변경 없음

--- src/pages/Products/Filter/ProductsFilter.js
+++ src/pages/Products/Filter/ProductsFilter.js
@@ -6,13 +6,14 @@ const ProductsFilter = ({ changeCheckStatus, filterName }) => {
   return (
     <div className="productFilter">
       <span className="product-type-filter">유형</span>
-      <input className="expand-input" id="expand-button" type="checkbox" />
+
       <label htmlfor="expand-button">
         <div className="expand-button-container">
           <div className="common-button first-button-element" />
           <div className="common-button second-button-element" />
           <div className="common-button third-button-element" />
         </div>
+        <input className="expand-input" id="expand-button" type="checkbox" />
       </label>
       <ul className="filtering-list">
         {filterName.map((subcategory, idx) => (
